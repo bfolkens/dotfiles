e6f54a250f7353265978eb5367493c53b4397b48
Bradford Folkens <i.am@bradfolkens.com>
2022-12-19 22:02:35 -0600

    Update config

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,7 +1,16 @@
 require('nvim-treesitter.configs').setup {
   -- Modules and its options go here
-  highlight = { enable = true },
+  highlight = {
+    enable = true,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false
+  },
   incremental_selection = { enable = true },
+  indent = { enable = true },
   refactor = {
     highlight_definitions = { enable = true },
     smart_rename = { enable = true },
@@ -9,4 +18,3 @@ require('nvim-treesitter.configs').setup {
   },
   textobjects = { enable = true },
 }
-
