8127925c5f8d1491dfb91daa66ae95ca835e1a91
Bradford Folkens <i.am@bradfolkens.com>
2023-06-17 08:35:08 -0500

    Turn off auto install - was getting annoying

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -7,10 +7,10 @@ require('nvim-treesitter.configs').setup {
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+  auto_install = false,
 
   -- List of parsers to ignore installing (for "all")
-  ignore_install = { "bash", "eex", "elixir", "html", "javascript", "lua" },
+  ignore_install = { "all" },
 
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
