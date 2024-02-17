bb6601750610514c1eda173cdf606660a09e15e0
Bradford Folkens <i.am@bradfolkens.com>
2024-02-17 12:53:56 -0600

    Added text-case plugin

--- nvim/lua/bradford/plugins.lua
+++ nvim/lua/bradford/plugins.lua
@@ -60,6 +60,17 @@ local plugins = {
 			}
 		end
 	},
+  {
+    "johmsalas/text-case.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("textcase").setup({})
+      require("telescope").load_extension("textcase")
+    end,
+    keys = {
+      { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "v" }, desc = "Telescope" },
+    },
+  },
 	'folke/twilight.nvim',
 	'folke/zen-mode.nvim',
 	-- {
