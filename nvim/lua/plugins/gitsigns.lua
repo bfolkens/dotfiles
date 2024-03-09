4fcc136bbecadc4989bbdf2991ef4eda31c8d444
Bradford Folkens <i.am@bradfolkens.com>
2024-03-08 22:44:17 -0600

    Added some gitsigns keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -4,5 +4,8 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		require('gitsigns').setup()
+
+		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+		vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", {})
 	end
 }
