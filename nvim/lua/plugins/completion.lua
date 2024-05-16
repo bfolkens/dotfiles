9a0d28b616b4074abb328c6683b6c396c08326d0
Bradford Folkens <i.am@bradfolkens.com>
2024-05-16 09:01:37 -0500

    Refactor a bit

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,13 +1,14 @@
 return {
   'hrsh7th/nvim-cmp',
-  event = { "BufReadPost", "BufNewFile" },
+  lazy = false,
+  priority = 100,
   dependencies = {
+    'onsails/lspkind.nvim',
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
-    'L3MON4D3/LuaSnip',
+    'hrsh7th/cmp-buffer',
+    { 'L3MON4D3/LuaSnip', build = "make install_jsregexp" },
     'saadparwaiz1/cmp_luasnip',
-    'onsails/lspkind-nvim'
   },
   config = function()
     local cmp = require("cmp")
