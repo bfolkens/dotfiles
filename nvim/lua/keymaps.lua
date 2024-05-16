41f26aad277b2ef42c33cdcd182d0ff4179b9995
Bradford Folkens <i.am@bradfolkens.com>
2024-05-16 08:53:06 -0500

    Refactor a bit

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,10 +8,12 @@ vim.g.mapleader = ";"
 --  term_mode = "t",
 --  command_mode = "c"
 
-vim.keymap.set("n", "<esc>", ":noh<CR>", { silent = true, desc = "Stop highlighting on Enter" })
+local set = vim.keymap.set
+
+set("n", "<esc>", ":noh<CR>", { silent = true, desc = "Stop highlighting on Enter" })
 
 -- Better window navigation
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { silent = true })
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { silent = true })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { silent = true })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { silent = true })
+set("n", "<C-l>", "<C-w><C-l>", { silent = true })
+set("n", "<C-h>", "<C-w><C-h>", { silent = true })
+set("n", "<C-j>", "<C-w><C-j>", { silent = true })
+set("n", "<C-k>", "<C-w><C-k>", { silent = true })
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,54 +1,56 @@
+local opt = vim.opt
+
 -- Set options
 
-vim.opt.termguicolors = true
-vim.opt.showmatch = true                -- Show matching brackets.
-vim.opt.ignorecase = true               -- Do case insensitive matching
-vim.opt.hlsearch = true                 -- Highlight search results
-vim.opt.tabstop = 2                     -- Number of columns occupied by a tab character
-vim.opt.showtabline = 2
-vim.opt.softtabstop = 2                 -- See multiple spaces as tabstops so <BS> does the right thing
-vim.opt.expandtab = true                -- Converts tabs to white space
-vim.opt.shiftwidth = 2                  -- Width for autoindents
-vim.opt.autoindent = true               -- Indent a new line the same amount as the line just typed
-vim.opt.number = true                   -- Add line numbers
-vim.opt.relativenumber = true
-vim.opt.encoding = "utf8"
-vim.opt.fileencoding = "utf8"
-vim.opt.termencoding = "utf8"
-vim.opt.ruler = true
-vim.opt.spelllang = "en"
-vim.opt.backup = false
-vim.opt.swapfile = false
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hidden = true                   -- Allows buffers to be hidden
-vim.opt.inccommand = "split"
-vim.opt.wildmenu = true
-vim.opt.wildmode = "longest:full,full"
+opt.termguicolors = true
+opt.showmatch = true                -- Show matching brackets.
+opt.ignorecase = true               -- Do case insensitive matching
+opt.hlsearch = true                 -- Highlight search results
+opt.tabstop = 2                     -- Number of columns occupied by a tab character
+opt.showtabline = 2
+opt.softtabstop = 2                 -- See multiple spaces as tabstops so <BS> does the right thing
+opt.expandtab = true                -- Converts tabs to white space
+opt.shiftwidth = 2                  -- Width for autoindents
+opt.autoindent = true               -- Indent a new line the same amount as the line just typed
+opt.number = true                   -- Add line numbers
+opt.relativenumber = true
+opt.encoding = "utf8"
+opt.fileencoding = "utf8"
+opt.termencoding = "utf8"
+opt.ruler = true
+opt.spelllang = "en"
+opt.backup = false
+opt.swapfile = false
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hidden = true                   -- Allows buffers to be hidden
+opt.inccommand = "split"
+opt.wildmenu = true
+opt.wildmode = "longest:full,full"
 -- Set via ~/.agignore
 -- vim.opt.wildignore = "*/.git/*,.data/*,*/tmp/*,*/node_modules/*,_build/*,dist/*,deps/*,priv/static/*,*.pyc,*.so,*.a,*.o,*.swp"
 vim.cmd("filetype on")                 -- Enable filetype detection
 vim.cmd("filetype plugin indent on")
-vim.opt.history = 1000                  -- Keep 1000 lines of command line history
-vim.opt.showcmd = true                  -- Display incomplete commands
-vim.opt.title = true                    -- Show title in console title bar
-vim.opt.ttyfast = true                  -- Smoother changes
-vim.opt.scrolloff = 3                   -- Keep 3 lines when scrolling
-vim.opt.laststatus = 3                  -- Global statusline
-vim.opt.signcolumn = "yes"              -- Keep the gutter open so it doesn't jar the screen
-vim.opt.cursorcolumn = true             -- Vertical cursor alignment column
--- vim.opt.winbar = "%f" nvim 0.8+
+opt.history = 1000                  -- Keep 1000 lines of command line history
+opt.showcmd = true                  -- Display incomplete commands
+opt.title = true                    -- Show title in console title bar
+opt.ttyfast = true                  -- Smoother changes
+opt.scrolloff = 3                   -- Keep 3 lines when scrolling
+opt.laststatus = 3                  -- Global statusline
+opt.signcolumn = "yes"              -- Keep the gutter open so it doesn't jar the screen
+opt.cursorcolumn = true             -- Vertical cursor alignment column
+-- opt.winbar = "%f" nvim 0.8+
 
 -- Config global undo
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
 -- Completion
-vim.opt.shortmess:append("c")
-vim.opt.shortmess:append("F")
-vim.opt.completeopt = {"menu", "menuone", "noselect"}
+opt.shortmess:append("c")
+opt.shortmess:append("F")
+opt.completeopt = {"menu", "menuone", "noselect"}
 
 -- Syntax Coloring
 vim.cmd("syntax on")
@@ -60,13 +62,13 @@ vim.cmd("syntax on")
 vim.cmd("syntax sync minlines=256")
 
 -- Folding
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter.foldexpr()"
-vim.opt.foldlevelstart = 4
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter.foldexpr()"
+opt.foldlevelstart = 4
 
 if vim.fn.has("windows") then
   -- Middle dot (U+00B7, UTF-8: C2 B7)
-  vim.opt.fillchars = { fold = "·" }
+  opt.fillchars = { fold = "·" }
 
   -- Current theme sets these, so ignore for now
   -- Box drawings heavy vertical (U+2503, UTF-8: E2 94 83)
