4c2909dafe681c2eef7d3ba1730ef1e876e76141
Bradford Folkens <hello@bradfolkens.com>
2025-03-11 08:29:32 -0600

    Add mini.surround with 'r' map

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -34,10 +34,22 @@ return {
       }
     }
   },
-  -- This clobbers 's', need to find a different keymap
-  -- {
-  --   'echasnovski/mini.surround',
-  --   event = "BufEnter",
-  --   config = {}
-  -- }
+  {
+    'echasnovski/mini.surround',
+    event = "BufEnter",
+    config = {
+      mappings = {
+        add = 'ra', -- Add surrounding in Normal and Visual modes
+        delete = 'rd', -- Delete surrounding
+        find = 'rf', -- Find surrounding (to the right)
+        find_left = 'rF', -- Find surrounding (to the left)
+        highlight = 'rh', -- Highlight surrounding
+        replace = 'rr', -- Replace surrounding
+        update_n_lines = 'rn', -- Update `n_lines`
+
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+    }
+  }
 }
