062cb30e3e5ea7bac0f2ed3cbc43c6cff411f5e8
Bradford Folkens <hello@bradfolkens.com>
2025-04-12 13:49:06 -0500

    Add elixir-ls config

---
+++ nvim/lsp/elixir-ls.lua
@@ -0,0 +1,6 @@
+return {
+  cmd = { '/Users/bfolkens/local/opt/elixir-ls/language_server.sh' },
+  filetypes = { 'elixir', 'eelixir', 'heex', 'surface' },
+  root_markers = { 'mix.exs', '.git' },
+  single_file_support = true,
+}
