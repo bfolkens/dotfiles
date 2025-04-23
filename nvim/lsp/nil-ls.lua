e483f23dcce89689119d520263e572a6e0c72773
Bradford Folkens <hello@bradfolkens.com>
2025-04-22 21:18:47 -0500

    Add formatter

--- nvim/lsp/nil-ls.lua
+++ nvim/lsp/nil-ls.lua
@@ -2,4 +2,12 @@ return {
   cmd = { 'nil' },
   filetypes = { 'nix' },
   root_markers = { 'flake.nix', '.git' },
+  settings = {
+    -- escape 'nil' bc it's a keyword
+    ['nil'] = {
+      formatting = {
+        command = { 'nixfmt' }
+      },
+    },
+  }
 }
