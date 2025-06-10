521a5a81d9c4e7ec3d50fc0c81c36124ea003051
Bradford Folkens <hello@bradfolkens.com>
2025-06-10 12:08:29 -0500

    Fix more borders, refactor lsp init config stuff into lua/lsp.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,12 +5,6 @@ require("highlight_yank")
 require("disable_new_line_comment")
 require("cursor_hold_highlight")
 require("lazy-bootstrap")
-
-
--- https://lugh.ch/switching-to-neovim-native-lsp.html
--- https://gpanders.com/blog/whats-new-in-neovim-0-11/#lspa
--- https://neovim.io/doc/user/lsp.html#lsp-config
-
-vim.lsp.enable({ 'sumneko', 'typescript-ls', 'elixir-ls', 'solargraph', 'tailwindcss', 'terraform-ls', 'nil-ls', 'texlab', 'postgres-lsp' })
+require("lsp")
 
 -- vim.lsp.set_log_level("trace")
---
+++ nvim/lua/lsp.lua
@@ -0,0 +1,20 @@
+-- https://lugh.ch/switching-to-neovim-native-lsp.html
+-- https://gpanders.com/blog/whats-new-in-neovim-0-11/#lspa
+-- https://neovim.io/doc/user/lsp.html#lsp-config
+
+vim.lsp.enable({
+  'elixir-ls',
+  'nil-ls',
+  'postgres-lsp',
+  'solargraph',
+  'sumneko',
+  'tailwindcss',
+  'terraform-ls',
+  'texlab',
+  'typescript-ls',
+})
+
+-- Add border to the diagnostic popup window
+vim.diagnostic.config({
+  float = { border = "rounded" },
+})
