a970aaba8e55f57e38b46135c5cc52c148140604
Bradford Folkens <hello@bradfolkens.com>
2025-11-03 08:48:11 -0600

    Upgrade psql LSP

--- nvim/lsp/postgres-lsp.lua
+++ nvim/lsp/postgres-lsp.lua
@@ -1,10 +1,14 @@
 ---@brief
 ---
---- https://pgtools.dev
+--- https://pg-language-server.com
 ---
 --- A collection of language tools and a Language Server Protocol (LSP) implementation for Postgres, focusing on developer experience and reliable SQL tooling.
+
+---@type vim.lsp.Config
 return {
-  cmd = { 'postgrestools', 'lsp-proxy' },
-  filetypes = { 'sql', },
-  root_markers = { 'postgrestools.jsonc' },
+  cmd = { 'postgres-language-server', 'lsp-proxy' },
+  filetypes = {
+    'sql',
+  },
+  root_markers = { 'postgres-language-server.jsonc' },
 }
