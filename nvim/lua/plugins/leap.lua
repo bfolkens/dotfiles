ffda358e6dcb8ade66032d744c523a01d55c2f44
Bradford Folkens <hello@bradfolkens.com>
2026-03-21 09:22:29 -0600

    Fixed leap after update

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -3,12 +3,17 @@ return {
   config = function()
     local leap = require('leap')
 
+    local pairs = {
+      { { "n", "x", "o" }, "f",  "<Plug>(leap-forward)",       "Leap forward" },
+      { { "n", "x", "o" }, "F",  "<Plug>(leap-backward)",      "Leap backward" },
+      { { "x", "o" },      "x",  "<Plug>(leap-forward-till)",  "Leap forward till" },
+      { { "x", "o" },      "X",  "<Plug>(leap-backward-till)", "Leap backward till" },
+      { { "n", "x", "o" }, "gs", "<Plug>(leap-from-window)",   "Leap from window" },
+      { { "n", "x", "o" }, "gs", "<Plug>(leap-cross-window)",  "Leap from window" }
+    }
+
     -- add_default_mappings (but with f/F instead of s/S)
-    for _, _1_ in ipairs({ { { "n", "x", "o" }, "f", "<Plug>(leap-forward-to)", "Leap forward to" }, { { "n", "x", "o" },
-      "F", "<Plug>(leap-backward-to)", "Leap backward to" }, { { "x", "o" }, "x", "<Plug>(leap-forward-till)",
-      "Leap forward till" }, { { "x", "o" }, "X", "<Plug>(leap-backward-till)", "Leap backward till" }, { { "n", "x", "o" },
-      "gs", "<Plug>(leap-from-window)", "Leap from window" }, { { "n", "x", "o" }, "gs", "<Plug>(leap-cross-window)",
-      "Leap from window" } }) do
+    for _, _1_ in ipairs(pairs) do
       local _each_2_ = _1_
       local modes = _each_2_[1]
       local lhs = _each_2_[2]
