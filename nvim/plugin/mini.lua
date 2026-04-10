0b8ae329efa95f4c62c53df3b2e2377db0c46ecc
Bradford Folkens <hello@bradfolkens.com>
2026-04-10 08:45:38 -0500

    Remove telescope in favor of mini.pick

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -6,8 +6,9 @@ vim.pack.add({
   -- 'nvim-mini/mini.pairs',
   'https://github.com/nvim-mini/mini.splitjoin',
   'https://github.com/nvim-mini/mini.move',
+  'https://github.com/nvim-mini/mini.pick',
   'https://github.com/nvim-mini/mini.surround',
-  'https://github.com/nvim-mini/mini.trailspace'
+  'https://github.com/nvim-mini/mini.trailspace',
 })
 
 require('mini.ai').setup()
@@ -26,18 +27,29 @@ require('mini.move').setup({
   }
 })
 
+require('mini.pick').setup({
+  mappings = {
+    choose_marked = '<C-q>'
+  }
+})
+
+vim.keymap.set("n", "<leader>b", '<Cmd>Pick buffers<CR>', { desc = "MiniPick - find buffers" })
+vim.keymap.set("n", "<leader>f", '<Cmd>Pick files<CR>', { desc = "MiniPick - find files" })
+vim.keymap.set("n", "<leader>h", '<Cmd>Pick help<CR>', { desc = "MiniPick - help tags" })
+vim.keymap.set("n", "<leader>a", '<Cmd>Pick grep_live<CR>', { desc = "MiniPick - live grep (rg) project" })
+
 require('mini.surround').setup({
   mappings = {
-    add = 'ra',                -- Add surrounding in Normal and Visual modes
-    delete = 'rd',             -- Delete surrounding
-    find = 'rf',               -- Find surrounding (to the right)
-    find_left = 'rF',          -- Find surrounding (to the left)
-    highlight = 'rh',          -- Highlight surrounding
-    replace = 'rr',            -- Replace surrounding
-    update_n_lines = 'rn',     -- Update `n_lines`
+    add = 'ra',            -- Add surrounding in Normal and Visual modes
+    delete = 'rd',         -- Delete surrounding
+    find = 'rf',           -- Find surrounding (to the right)
+    find_left = 'rF',      -- Find surrounding (to the left)
+    highlight = 'rh',      -- Highlight surrounding
+    replace = 'rr',        -- Replace surrounding
+    update_n_lines = 'rn', -- Update `n_lines`
 
-    suffix_last = 'l',         -- Suffix to search with "prev" method
-    suffix_next = 'n',         -- Suffix to search with "next" method
+    suffix_last = 'l',     -- Suffix to search with "prev" method
+    suffix_next = 'n',     -- Suffix to search with "next" method
   }
 })
 
--- nvim/plugin/text-case.lua
+++ nvim/plugin/text-case.lua
@@ -1,4 +1,3 @@
 vim.pack.add({ 'https://github.com/johmsalas/text-case.nvim' })
 
 require("textcase").setup({})
-require("telescope").load_extension("textcase")
