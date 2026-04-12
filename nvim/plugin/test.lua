56a23de6f7356481cf7cf527ad2549097da19015
Bradford Folkens <hello@bradfolkens.com>
2026-04-12 10:43:49 -0500

    Add neotest

--- nvim/plugin/test.lua
+++ nvim/plugin/test.lua
@@ -1,12 +1,27 @@
-vim.pack.add({ 'https://github.com/vim-test/vim-test' })
+-- vim.pack.add({ 'https://github.com/vim-test/vim-test' })
 
-vim.g["test#strategy"] = "basic"
-vim.g["test#elixir#exunit#executable"] = "MIX_ENV=test mix test"
+-- vim.g["test#strategy"] = "basic"
+-- vim.g["test#elixir#exunit#executable"] = "MIX_ENV=test mix test"
 
-vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", { desc = "Test nearest" })
-vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", { desc = "Test entire file" })
-vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>", { desc = "Test suite" })
-vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", { desc = "Test last" })
-vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>", { desc = "Test visit" })
+vim.pack.add({
+  'https://github.com/nvim-neotest/nvim-nio',
+  'https://github.com/nvim-neotest/neotest',
+  'https://github.com/jfpedroza/neotest-elixir',
+  'https://github.com/Jumziey/neotest-nix-unit.nvim'
+})
+
+-- NOTE: If a language isn't working, check to make sure a treesitter parser is installed for it.
+
+local nt = require("neotest")
+
+nt.setup({
+  adapters = {
+    require("neotest-elixir"),
+    require("neotest-nix-unit"),
+  },
+})
+
+vim.keymap.set("n", "<leader>t", function() nt.run.run() end, { desc = "Test nearest" })
+vim.keymap.set("n", "<leader>tf", function() nt.run.run(vim.fn.expand("%")) end, { desc = "Test entire file" })
 
 vim.keymap.set("t", "<C-o>", "<C-\\><C-n>", { silent = true, desc = "Go back to normal mode from terminal mode" })
